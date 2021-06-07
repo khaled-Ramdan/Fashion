afa8fadef09987b613fedb5b7735998c6e7a77dc
mohamedadel46 <65991757+mohamedadel46@users.noreply.github.com>
2021-06-07 16:20:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const faceImg = document.querySelector(".model_face");
+var navList = document.getElementById("nav-lists");
+
+const faces = [
+    "./images/ronaldo.png",
+    "./images/pexels-lena-hsvl-6851541.jpg",
+    "./images/Benzema.png",
+    "./images/pexels-lena-hsvl-6691175.jpg"
+]
+
+let DURATION = 3000
+let activeImageLength = 0
+faceImg.src = faces[activeImageLength];
+
+const interval = setInterval(() => {
+    if (activeImageLength !== faces.length) {
+        faceImg.src = faces[activeImageLength];
+        activeImageLength++;
+    }
+    else {
+        activeImageLength = 0
+        faceImg.src = faces[activeImageLength++];
+    }
+}, DURATION);
+
+function Show() {
+navList.classList.add("_Menus-show");
+}
+
+function Hide(){
+navList.classList.remove("_Menus-show");
+}
